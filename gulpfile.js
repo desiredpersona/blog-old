03aaf0fcc81b4649e2bb5b16a85f5f4bcaa09446
desiredpersona <desiredpersona@gmail.com>
2018-03-18 23:01:07 +0000

    Add dependencies for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const purgecss = require('gulp-purgecss')
 
+// Run `gulp purgecss` to remove unused CSS.
 gulp.task('purgecss', () => {
   return gulp
     .src('_site/**/*.css')
